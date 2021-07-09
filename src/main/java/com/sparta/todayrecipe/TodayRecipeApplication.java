0f05194b922e396dba15fb809be5103b532b49d8
Code-Angler <xkqkrtk@naver.com>
2021-07-09 15:00:40 +0900

    Initial commit

---
+++ src/main/java/com/sparta/todayrecipe/TodayRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todayrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/todayrecipe/TodayRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.todayrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
