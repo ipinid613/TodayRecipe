880ee2d14e5e6d64615fd007c98b30e06c542f36
Code-Angler <xkqkrtk@naver.com>
2021-07-10 03:03:35 +0900

    로그인 파트 구현 (유효성, 카카오톡 로그인 확인완료). 성경 commit

---
+++ src/main/java/com/sparta/todayrecipe/Dto/SignupRequestDto.java
@@ -0,0 +1,37 @@
+package com.sparta.todayrecipe.Dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+
+@Setter
+@Getter
+public class SignupRequestDto {
+    private Long id;
+
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{3,12}",
+            message = "유저명은 영문 대,소문자와 숫자 적어도 1개 이상씩 포함된 3자이사 12자 이하 여야 합니다.")
+    @NotBlank(message = "유저명은 필수 입력 값입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp="(?=\\S+$).{4,10}",
+            message = "비밀번호는 4 ~ 10자의 비밀번호여야 합니다.")
+    private String password;
+    @NotBlank(message = "비밀번호는 확인이 필요합니다.")
+    private String repassword;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
+    private String email;
+
+    private boolean admin = false;
+
+    private String adminToken = "";
+
+}
---
+++ src/main/java/com/sparta/todayrecipe/security/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.sparta.todayrecipe.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                // image 폴더를 login 없이 허용
+                .antMatchers("/images/**").permitAll()
+                // css 폴더를 login 없이 허용
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/user/**").permitAll()
+                .antMatchers("/api/notices").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/").permitAll()
+                // 그 외 모든 요청은 인증과정 필요
+//                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/user/login")
+                .loginProcessingUrl("/user/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/user/login/error")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/user/logout")
+                .permitAll();
+    }
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
