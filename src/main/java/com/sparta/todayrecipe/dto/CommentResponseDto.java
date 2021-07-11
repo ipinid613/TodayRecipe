1b62e103482647fa61ca4b9ef4861b09ea53fe2f
BlossomWhale <85476751+BlossomWhale@users.noreply.github.com>
2021-07-11 23:21:51 +0900

    add CommentResponseDto

---
+++ src/main/java/com/sparta/todayrecipe/dto/CommentResponseDto.java
@@ -0,0 +1,25 @@
+package com.sparta.todayrecipe.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+
+public class CommentResponseDto {
+    private final Long id;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
+    public CommentResponseDto(Long id, String content, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+
+
+}
