ae0f02420cd731da450f30114a788b7252828412
Code-Angler <xkqkrtk@naver.com>
2021-07-12 18:33:47 +0900

    add cors

--- src/main/java/com/sparta/todayrecipe/repository/CommentRepository.java
+++ src/main/java/com/sparta/todayrecipe/repository/CommentRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-    List<Comment> findByArticle(Article article);
-
     List<Comment> findByArticle_Id(Long articleId);
 }
