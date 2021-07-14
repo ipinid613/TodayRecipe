e43872366b689750f395c93ed5cc20f9bcf2f9a2
ipinid613 <ipinid613@kakao.com>
2021-07-14 11:44:39 +0900

    Pull 이후 삭제된 코드에 대한 보존처리(추후 참고용)

--- src/main/java/com/sparta/todayrecipe/service/UserService.java
+++ src/main/java/com/sparta/todayrecipe/service/UserService.java
@@ -49,6 +49,20 @@ public class UserService {
             throw new UserRequestException(errorMessage);
         }
     }
+    ////// 기존 코드 //////
+    //public Map<String, String> validateHandling(Errors errors) {
+    //        Map<String, String> validatorResult = new HashMap<>();
+    //        for (FieldError error : errors.getFieldErrors()) {
+    //            // errors.getFieldErrors() :
+    //            // 입력 정보(아이디, 비밀번호, 비번 재확인, 이메일)의 유효성검사 결과에 대한 오류값(ex : 공백이 있다거나, 정규식 위반하는 경우들)
+    //            // 여러 오류값들을 for문을 돌며 하나 하나씩 꺼냄(ex : 아이디 유효성검사 결과 오류 하나)
+    //            String validKeyName = String.format("error", error.getField());
+    //            // 하나의 오류값을 format처리하여 validKeyName에 저장. / ex) {valid_username : "유저명은 필수 입력 값입니다"}
+    //            validatorResult.put(validKeyName, error.getDefaultMessage());
+    //        }
+    //        return validatorResult; // 유효성검사 통과 시 {msg : null} 반환하고 db에 저장됨.
+    //    }
+
 
     // 회원가입
     public void registerUser(@Valid @RequestBody SignupRequestDto signupRequestDto) {
@@ -70,15 +84,44 @@ public class UserService {
             errorMessage = "비밀번호가 일치하지 않습니다.";
             throw new UserRequestException(errorMessage);
         }
-
         // 패스워드 인코딩
         String password = passwordEncoder.encode(signupRequestDto.getPassword());
         /// 위의 조건을 다 통과한 경우에 한해 userRepository.save 가능함 ///
         String email = signupRequestDto.getEmail();
         User user = new User(username, password, email);
         userRepository.save(user);
-
     }
+    ////// 기존코드 //////
+//    // 회원가입
+//    public String registerUser(@Valid @RequestBody SignupRequestDto signupRequestDto) {
+//        String username = signupRequestDto.getUsername();
+//        String errorMessage = null;
+//        // 회원 ID 중복 확인
+//        Optional<User> found = userRepository.findByUsername(username);
+//        if (found.isPresent()) {
+//            errorMessage = "중복된 사용자 ID 가 존재합니다.";
+////            throw new IllegalArgumentException(errorMessage);
+//            return errorMessage;
+//        }
+//        // 패스워드 속에 아이디값 중복 없애기
+//        if(signupRequestDto.getPassword().contains(username) || username.contains(signupRequestDto.getPassword())) {
+//            errorMessage = "ID을 포함한 비번은 사용불가합니다.";
+//            return errorMessage;
+//        }
+//        if (!signupRequestDto.getPassword().equals(signupRequestDto.getRepassword())) {
+//            errorMessage = "비밀번호가 일치하지 않습니다.";
+//            return errorMessage;
+//        }
+//        // 패스워드 인코딩
+//        String password = passwordEncoder.encode(signupRequestDto.getPassword());
+//        /// 위의 조건을 다 통과한 경우에 한해 userRepository.save 가능함 ///
+//        String email = signupRequestDto.getEmail();
+//        User user = new User(username, password, email);
+//        userRepository.save(user);
+//        return errorMessage;
+//    }
+
+
 
     public void kakaoLogin(String authorizedCode) {
         // 카카오 OAuth2 를 통해 카카오 사용자 정보 조회
